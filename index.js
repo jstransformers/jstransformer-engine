39dc189ebf36cb82645a453f99659fe2e18e2bfd
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-14 12:16:49 +0200

    feat: updates for latest Engine v1
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const engine = require('engine')
+const Engine = require('engine')
 
 exports.name = 'engine'
 exports.outputFormat = 'html'
@@ -8,32 +8,28 @@ exports.outputFormat = 'html'
 /**
  * Register all the provided helpers.
  */
-function registerHelpers(engine, helpers) {
+function register(engine, helpers) {
   // Add all the helpers.
   for (const name in helpers || {}) {
     if ({}.hasOwnProperty.call(helpers, name)) {
-      let helper = null
-      switch (typeof helpers[name]) {
-        case 'string':
-          // eslint-disable-next-line import/no-dynamic-require
-          helper = require(helpers[name])
-          break
-        case 'function':
-        default:
-          helper = helpers[name]
-          break
+      if (typeof helpers[name] === 'string') {
+        // eslint-disable-next-line import/no-dynamic-require
+        engine.helper(name, require(helpers[name]))
+        continue
       }
-      if (helper !== null) {
-        engine.helper(name, helper)
+      if (typeof helpers[name] === 'function') {
+        engine.helper(name, helpers[name])
+        continue
       }
+      throw new Error('Invalid helpers type')
     }
   }
 }
 
 exports.compile = function (str, options) {
-  const opts = options || {}
-  const eng = engine(opts)
-  registerHelpers(eng, opts.helpers)
-  registerHelpers(eng, opts.partials)
+  const opts = Object.assign({}, options)
+  const eng = new Engine(opts)
+
+  register(eng, opts.helpers)
   return eng.compile(str, opts)
 }
